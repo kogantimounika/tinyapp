95ef9fc0d5398212659d6dd3d45d4e9597736d7b
kogantimounika <kogantimounica@gmail.com>
2019-12-10 00:39:34 +0000

    tiny app

--- express_server.js
+++ express_server.js
@@ -8,6 +8,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls",(req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+ // res.render("urls_index",urlDatabase);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
