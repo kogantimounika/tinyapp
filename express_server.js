bef08e598aadfa382c68568e368c3807b6f7db52
kogantimounika <kogantimounica@gmail.com>
2019-12-10 03:44:20 +0000

    tiny app

--- express_server.js
+++ express_server.js
@@ -2,17 +2,26 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
 app.set("view engine", "ejs");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls",(req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
- // res.render("urls_index",urlDatabase);
-});
+function generateRandomString(outputLength) {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < outputLength; i ++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  console.log("six digit code: ", result);
+  return result;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -30,8 +39,24 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls",(req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+ // res.render("urls_index",urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca"  /* What goes here? */ };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(generateRandomString(6));         // Respond with 'Ok' (we will replace this)
 });
 
