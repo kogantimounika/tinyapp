272d7309a9d23e8aff7eed152d121ee6a573bb1d
kogantimounika <kogantimounica@gmail.com>
2020-01-12 00:27:43 +0000

    duplicate data

---
+++ sample.js
@@ -0,0 +1,25 @@
+let urlDatabase = {
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
+};
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  },
+  "aJ48lW": {
+    id: "aJ48lW",
+    email: "z@z",
+    password: "z"
+  },
+};
+
+exports.users = users;
+exports.longURL = urlDatabase;
